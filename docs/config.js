d0ccd11348ca9ed3b901a92088e320dd854097d2
ZacharyDLopez <143556950+ZacharyDLopez@users.noreply.github.com>
2024-12-03 16:27:54 -0500

    Added a fresh API key for the presentation

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiKey: "bLvh7j17ae3uzYRfMX2pbe49MFFCy4blMIXUzNZFCAB3JWNaQk"
+    //apiKey: "bLvh7j17ae3uzYRfMX2pbe49MFFCy4blMIXUzNZFCAB3JWNaQk"
+    apiKey: "XfyrIiJFL27KoepuOCjcbR91QlGbk2IimrsQjCteNh5Zv9e4Bp"
 }
 
 export default config;
